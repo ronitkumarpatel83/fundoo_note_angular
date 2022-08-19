a4d922f7a796e591f6f9b55cf5c1f62c25e4e730
ronitkumarpatel83 <ronitkumarpatel83@gmail.com>
2022-08-19 23:59:58 +0530

    [Ronit]Added: creating register page and login page

--- FundooNote/src/app/app.module.ts
+++ FundooNote/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -15,10 +18,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     LoginComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FlexLayoutModule,
-    BrowserAnimationsModule
+    BrowserModule,AppRoutingModule,FlexLayoutModule,
+    BrowserAnimationsModule,MatFormFieldModule,
+    MatInputModule,MatCheckboxModule,ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- FundooNote/src/app/components/register/register.component.ts
+++ FundooNote/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  registerForm!: FormGroup;
+  submitted = false;
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.registerForm = this.formBuilder.group({
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      cpassword: ['', Validators.required],
+  },);
   }
+
+  onSubmit() {
+    if(this.registerForm.valid){
+      console.log("Valid data", this.registerForm.value)
+      // let data = {
+      //   firstname: this.registerForm.value.firstname,
+      //   lastname: this.registerForm.value.lastname,
+      //   username: this.registerForm.value.username,
+      //   password: this.registerForm.value.password,
+      // //   email: 
+      // //   phone_number: 
+      // //   location: 
+      // }
+    }else{
+      console.log("Invalid data", this.registerForm.value)
+    }
+    
+}
 
 }
